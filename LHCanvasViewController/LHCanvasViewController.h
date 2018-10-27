d026fbbc322dd141d7498055b541c63815a4f404
Li-Heng Hsu <yesleon@me.com>
2018-10-27 08:28:56 +0800

    Initial Commit

---
+++ LHCanvasViewController/LHCanvasViewController.h
@@ -0,0 +1,19 @@
+//
+//  LHCanvasViewController.h
+//  LHCanvasViewController
+//
+//  Created by 許立衡 on 2018/10/27.
+//  Copyright © 2018 narrativesaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LHCanvasViewController.
+FOUNDATION_EXPORT double LHCanvasViewControllerVersionNumber;
+
+//! Project version string for LHCanvasViewController.
+FOUNDATION_EXPORT const unsigned char LHCanvasViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHCanvasViewController/PublicHeader.h>
+
+
